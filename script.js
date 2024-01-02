97d9e4eabec8df2b17ad95cefa862d10fcd58cdb
SalmaFarid998 <you@example.com>
2024-01-03 00:09:29 +0200

    Added location feature

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ let latitude = ""
 let longitude = ""
 let mylocation = ""
 
-getWeather()
-async function getWeather(){
+async function execute(){
+    await getLocation();
+     getWeather()
+}
+execute()
+ function getWeather(){
+    return new Promise(async function(){
     let city = document.querySelector("#cityinput").value
-    await this.getLocation()
-    
-    let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c43e0586fa84e79823151555233012&q=${latitude},${longitude}&days=3`)
-    console.log(mylocation)
-
-    if(city == ""){
-        if(latitude==""){
-            return false
-        }else{
-        let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c43e0586fa84e79823151555233012&q=${latitude},${longitude}&days=3`)
-    }
+    if(city !== ""){
+        var response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c43e0586fa84e79823151555233012&q=${city}&days=3`)
     }else{
-        let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c43e0586fa84e79823151555233012&q=${city}&days=3`)
+        var response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c43e0586fa84e79823151555233012&q=${latitude},${longitude}&days=3`)
     }
-    response = await response.json()
+    var response = await response.json()
     let location = response.location
     let weather = response.forecast.forecastday
     console.log(weather)
@@ -32,6 +28,7 @@ async function getWeather(){
         </div>`
     }
     document.querySelector(".forecast").innerHTML = divs
+    })
     
 }
 
@@ -41,13 +38,14 @@ async function getWeather(){
 document.querySelector("#search").addEventListener("click", getWeather)
 
 function getLocation() {
-    if (navigator.geolocation) {
-      mylocation = navigator.geolocation.getCurrentPosition(showPosition);
-    } 
-  }
- function showPosition(position) {
-    latitude =  position.coords.latitude 
-    longitude =  position.coords.longitude
-    // console.log(latitude)
-   
-  }
+    return new Promise(function(func){
+            navigator.geolocation.getCurrentPosition(function(position){
+            latitude =  position.coords.latitude 
+            longitude =  position.coords.longitude
+            console.log("show position done")
+            func()
+            });
+    })
+
+    }
+ 
