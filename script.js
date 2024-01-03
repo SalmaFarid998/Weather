c702916143afe5db14335716f1036fbdad19f093
SalmaFarid998 <you@example.com>
2024-01-03 21:52:52 +0200

    added more data and improved design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let latitude = ""
 let longitude = ""
 let mylocation = ""
-
+let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 async function execute(){
     await getLocation();
      getWeather()
@@ -16,15 +16,38 @@ execute()
         var response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=6c43e0586fa84e79823151555233012&q=${latitude},${longitude}&days=3`)
     }
     var response = await response.json()
+    let lightstatus = response.current.is_day
+    if(lightstatus == 0){
+        document.querySelector("body").classList.add("dark")
+    }
+    
     let location = response.location
     let weather = response.forecast.forecastday
-    console.log(weather)
-    let divs = "";
+    
+    document.querySelector(".city").innerHTML = location.name
+    let divs = ``;
     for (let i = 0; i<weather.length; i++){
+        let rain = weather[i].day.daily_will_it_rain
+    if (rain !== 0){
+        rainIcon = "fa-solid fa-cloud-rain"
+        rainState = "Rain"
+    }else{
+        rainIcon = "fa-solid fa-cloud"
+        rainState = "No Rain"
+    }
+
+        const d = new Date(weather[i].date);
+        let weekday = d.getDay()
         divs += `<div class="day">
-        <p> ${weather[i].day.maxtemp_c} </p>
+        <p>${week[weekday]} ${d.getDate()}-${d.getMonth()+1}-${d.getFullYear()}</p>
+        
         <img src="${weather[i].day.condition.icon}">
-        <p>${location.name}</p>
+        <p class="temp"> ${weather[i].day.avgtemp_c}°C </p>
+        <p class="status">${weather[i].day.condition.text}</p>
+        
+        
+        <span><i class="${rainIcon}"></i> ${rainState}</span> |
+        <span><i class="fa fa-wind"></i> ${weather[i].day.avgvis_km}kph</span>
         </div>`
     }
     document.querySelector(".forecast").innerHTML = divs
